347db4a28d76e3a71decc67abb35d0883247a273
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-07-15 20:18:49 +0800

    feat: 封装 Header 组件

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,29 @@ import type { FC } from 'react'
 
 import DarkModeSwitch from '@/components/DarkModeSwitch'
 
-interface HeaderProps {}
+import Divider from '../Divider'
+import SocialLink from '../SocialLink'
 
-const Header: FC<HeaderProps> = () => {
+interface HeaderProps {
+  title: string
+}
+
+const Header: FC<HeaderProps> = (props) => {
+  const { title } = props
+
   return (
-    <header>
-      <DarkModeSwitch />
+    <header className="flex justify-between px-8">
+      {/* 标题 */}
+      <section className="flex justify-center items-center h-16">{title}</section>
+
+      {/* 内容 */}
+      <section className="flex justify-center items-center h-16">
+        <DarkModeSwitch />
+
+        <Divider />
+
+        <SocialLink icon="mdi:github" link="https://github.com/Plasticine-Yang/astro-docsite-template" />
+      </section>
     </header>
   )
 }
